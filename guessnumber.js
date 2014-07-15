4147ab144a670d29d79008ff5b88f16478389158
Devin Dreszer <devindreszer@gmail.com>
2014-07-15 10:34:37 -0400

    Program ends and reveals the random number after three incorrect guesses

--- guessnumber.js
+++ guessnumber.js
@@ -2,11 +2,19 @@ var random = Math.floor((Math.random() * 10) + 1);
 
 var guess = prompt("Guess a number between 1 and 10");
 
-while(guess != random) {
+var remainingGuesses = 2;
+
+while(guess != random && remainingGuesses > 0) {
   if(guess == random) {
     alert("Your guess is correct!");
   } else {
     alert("Your guess is incorrect!");
     guess = prompt("Guess again");
+    remainingGuesses -= 1;
   }
 }
+
+if(guess != random) {
+  alert("Your guess is incorrect! The correct number was " + random + ".");
+}
+
