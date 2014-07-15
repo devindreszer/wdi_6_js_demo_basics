05daa2a52304416a37d29142ed28223b112f7b9d
Devin Dreszer <devindreszer@gmail.com>
2014-07-15 10:55:22 -0400

    App handles no response from user

--- guessnumber.js
+++ guessnumber.js
@@ -5,15 +5,16 @@ var guess = prompt("Guess a number between 1 and 10");
 var remainingGuesses = 2;
 
 while(guess !== random && remainingGuesses > 0) {
-  if (guess > random) {
+  if(guess === undefined) {
+    guess = prompt("Guess a number between 1 and 10");
+  } else if(guess > random) {
     alert("Your guess is too high.");
     guess = prompt("Guess lower");
-    remainingGuesses -= 1;
   } else {
     alert("Your guess is too low.");
     guess = prompt("Guess higher");
-    remainingGuesses -= 1;
   }
+  remainingGuesses -= 1;
 }
 
 if(parseInt(guess) === random) {
