0ea6697e15a484ca5196b49b5006e24ac5e46486
Devin Dreszer <devindreszer@gmail.com>
2014-07-15 10:10:09 -0400

    Program generates a random number and prompts the user to guess it

---
+++ guessnumber.js
@@ -0,0 +1,5 @@
+var random = Math.floor((Math.random() * 10) + 1);
+
+var guess = prompt("Guess a number between 1 and 10");
+
+
--- testrun.js
+++ testrun.js
@@ -1,1 +1,8 @@
-console.log('It works!');
+var holyNumber = prompt('What number did you count to?');
+if(holyNumber > 3) {
+  alert('Four shalt thou not count. Five is right out.');
+} else if(holyNumber < 3) {
+  alert('Count neither one nor two, excepting that thou then proceedest to three.');
+} else {
+  alert('Throw the holy hand grenade!');
+}
