300f3088f5d0a970e461f5b80dddff187ea0874c
Devin Dreszer <devindreszer@gmail.com>
2014-07-15 10:12:12 -0400

    Program tells the user whether a guess was correct or not

--- guessnumber.js
+++ guessnumber.js
@@ -2,4 +2,8 @@ var random = Math.floor((Math.random() * 10) + 1);
 
 var guess = prompt("Guess a number between 1 and 10");
 
-
+if(guess == random) {
+  alert("Your guess is correct!");
+} else {
+  alert("Your guess is incorrect!");
+}
